7f19d4593999ea8f5c8b23730973986621173afd
Jack <108418371+ZolarEclipse@users.noreply.github.com>
2024-08-02 11:55:20 -0500

    finished controllers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,64 +1,41 @@
-const router = require("express").Router();
-const { Product, Seller, Buyer, Order, Users, SellerProduct  } = require("../models");
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
 
-//Display all categories of items being sold on homepage
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const allProducts = await Product.findAll({});
-
-    const products = allProducts.map((product) => product.get({ plain: true }));
-
-    res.render("homepage", {
-      products,
+    const postData = await Post.findAll({
+      include: [User],
     });
-  } catch (err) {
-    console.log(err);
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+//render homepage
+    res.render('homepage', {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+ } catch (err) {
     res.status(500).json(err);
-  }
-});
+  }});
 
-//Display all products being sold on product page
-router.get("/product", async (req, res) => {
+// Single post 
+router.get('/post/:id', async (req, res) => {
   try {
-    const allProducts = await Product.findAll({});
-    console.log("Im working");
+    const postData = await Post.findByPk(req.params.id, {
+      include: [User, { model: Comment, include: [User] }],});
 
-    const products = allProducts.map((product) => product.get({ plain: true})); 
+    if (!postData) {
+      res.status(404).json({ message: "can not find post" });
+      return;
+    }
 
-    res.render('product', {
-      products,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+    const post = postData.get({ plain: true });
 
-//Display form to sell products and products being sold by user
-router.get('/profile', async (req, res) => {
-  try {
-    const profileData = await Users.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Product, SellerProduct, Seller, Buyer, Order }]
-    });
-
-    const profile = profileData.get({ plain: true });
-
-    res.render('profile', {
-      ...profile,
+    res.render('single-post', {
+      post,
+      loggedIn: req.session.loggedIn,
     });
   } catch (err) {
     res.status(500).json(err);
-  }
-});
-
-
-//Login route and redirect if already logged in
-router.get('/login', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
-  res.render('login');
-})
+  }});
 
 module.exports = router;
